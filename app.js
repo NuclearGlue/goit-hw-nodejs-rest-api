b1bfdedd62ab3f8869bd4ad5566d4e0c081729aa
Alexandr Sorokin <sanek1678@gmail.com>
2022-12-30 20:05:37 +0200

    All requests working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const contactsRouter = require('./routes/api/contacts');
 
 const app = express();
@@ -12,11 +14,15 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/contacts', contactsRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+app.get('/', (req, res) => {
+  res.status(200).send('<h2>Homepage</h2>');
 });
+
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Not found 404' });
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,95 +1,18 @@
-const Joi = require('joi');
 const express = require('express');
-const contacts = require('../../models/contacts.js');
+const ctrlContact = require('../../controllers/controller.js');
+
 const router = express.Router();
 
-const addValidate = Joi.object({
-  name: Joi.string().min(2).max(40).required(),
-  email: Joi.string().required(),
-  phone: Joi.number().required(),
-});
+router.get('/', ctrlContact.getAll);
 
-const updateValidate = Joi.object({
-  name: Joi.string().min(2).max(40),
-  email: Joi.string(),
-  phone: Joi.number(),
-});
+router.get('/:contactId', ctrlContact.getContactById);
 
-router.get('/', async (req, res, next) => {
-  try {
-    const allContacts = await contacts.listContacts();
-    res.status(200).json(allContacts);
-  } catch (e) {
-    res.status(404).json({ message: 'No contacts found' });
-    next(e);
-  }
-});
+router.post('/', ctrlContact.addNewContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await contacts.getContactById(req.params.contactId);
-    if (!contact) {
-      throw new Error();
-    }
-    res.status(200).json(contact);
-  } catch (e) {
-    res.status(404).json({ message: 'Contact with such index not found!' });
-    next(e);
-  }
-});
+router.put('/:contactId', ctrlContact.updateContact);
 
-router.post('/', async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const { error } = addValidate.validate(req.body);
+router.delete('/:contactId', ctrlContact.deleteContact);
 
-  try {
-    if (error) {
-      throw error;
-    }
-    const contact = await contacts.addContact(name, email, phone);
-    if (!contact) {
-      throw new Error('This contact is already in your contact list');
-    }
-    res.status(201).json({ contact: contact });
-  } catch (e) {
-    res.status(400).json({ message: e.message });
-    next(e);
-  }
-});
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await contacts.removeContact(req.params.contactId);
-    if (!contact) {
-      throw new Error();
-    }
-    res.status(200).json({ message: 'Success! Contact deleted!' });
-  } catch {
-    res.status(404).json({ message: 'Contact with such index not found!' });
-    next();
-  }
-});
-
-router.put('/:contactId', async (req, res, next) => {
-  const { error, value } = updateValidate.validate(req.body);
-  try {
-    if (error || Object.keys(value).length === 0) {
-      const message = error ? error.message : 'Some fields are missing';
-      res.status(400).json(message);
-      return;
-    }
-    const contact = await contacts.updateContact(
-      req.params.contactId,
-      req.body,
-    );
-    if (contact === null) {
-      throw new Error('Not found');
-    }
-    res.json(contact);
-  } catch (e) {
-    res.status(404).json({ message: e.message });
-    next(e);
-  }
-});
+router.patch('/:contactId/favorite', ctrlContact.updateContactStatus);
 
 module.exports = router;
